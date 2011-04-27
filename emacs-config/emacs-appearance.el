f3a3d2e798878aef60eb7efb38e93581547b5bee
antham <hamonanth@gmail.com>
2011-04-28 00:55:23 +0200

    Change emacs theme

--- emacs-config/emacs-appearance.el
+++ emacs-config/emacs-appearance.el
@@ -2,7 +2,7 @@
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 (if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
 
-(color-theme-initialize)
-(color-theme-billw)
-
 (set-cursor-color "yellow")
+
+(color-theme-initialize)
+(color-theme-arjen)
