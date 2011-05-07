9903bdc5961209d417ac328e2adbb21c4c92efe1
antham <hamonanth@gmail.com>
2011-05-07 17:33:15 +0200

    Add comment to projects/init.el file

---
+++ projects/init.el
@@ -0,0 +1,21 @@
+;; Create a elisp file, for instance example.el,
+;; in .emacs.d/projects folder with project config as follow
+;;
+;; (project-def "example"
+;; 	     '((basedir          "/home/path/example")
+;; 	       (src-patterns     ("*.c"))
+;; 	       (ignore-patterns  ("*.o"))
+;; 	       (tags-file        "/home/path/example/TAGS")
+;; 	       (file-list-cache  "/tmp/files")
+;; 	       (open-files-cache "/tmp/open-files")
+;; 	       (vcs              git)
+;; 	       (compile-cmd      "gcc")
+;; 	       (ack-args         "--cc")
+;; 	       (startup-hook     nil)
+;; 	       (shutdown-hook    nil)
+;; 	       ))
+;;
+;; And add it to your project config by appending following line below to this file
+;; (load-file "~/.emacs.d/projects/example.el")
+
+
