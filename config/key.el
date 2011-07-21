8dad1c6c7f2a5cd020bbbc90eb37ec21bec0b187
antham <hamonanth@gmail.com>
2011-07-21 02:20:12 +0200

    Add a binding to base-anything

--- config/key.el
+++ config/key.el
@@ -1,7 +1,7 @@
 (global-set-key "\C-x\C-m" 'execute-extended-command)
 (global-set-key "\C-c\C-m" 'execute-extended-command)
 
-(global-set-key (kbd "C-x b") 'base-anything)
+(global-set-key (kbd "C-;") 'base-anything)
 (global-set-key (kbd "C-x C-b") 'ibuffer)
 
 (global-set-key (kbd "C-c a a") 'anything-ack)
