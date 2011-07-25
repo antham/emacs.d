57f0e7506c4af3f0a9878ed513c02665d71b6955
antham <hamonanth@gmail.com>
2011-07-25 13:03:24 +0200

    Auto-complete only after 5 characters

--- config/custom.el
+++ config/custom.el
@@ -3,6 +3,7 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
+ '(ac-auto-start 5)
  '(ac-candidate-limit nil)
  '(ac-stop-words (quote ("$this")))
  '(ac-use-overriding-local-map nil)
