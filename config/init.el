f326d9c822aae05698ddd23006b4ae0b5ea15233
antham <hamonanth@gmail.com>
2011-08-28 20:12:30 +0200

    Add a function see file name and copy it to kill ring

--- config/init.el
+++ config/init.el
@@ -60,6 +60,7 @@
 ;; Functions
 (load-file "~/.emacs.d/functions/indent.el")
 (load-file "~/.emacs.d/functions/insert-text.el")
+(load-file "~/.emacs.d/functions/file.el")
 (load-file "~/.emacs.d/functions/misc.el")
 (load-file "~/.emacs.d/functions/term.el")
 
