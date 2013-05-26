5f5ec55acb0e4d74714ed27d4be64d976f182095
antham <hamonanth@gmail.com>
2013-05-26 23:38:48 +0200

    [Package manager] Add marmalade

--- init.el
+++ init.el
@@ -1,4 +1,10 @@
 (setq custom-file "~/.emacs.d/config/custom.el")
 (load custom-file)
 
-(load-file "~/.emacs.d/config/init.el")
+(require 'package)
+(add-to-list 'package-archives
+    '("marmalade" .
+      "http://marmalade-repo.org/packages/"))
+(package-initialize)
+
+(add-hook 'after-init-hook (lambda () (load-file "~/.emacs.d/config/init.el")))
