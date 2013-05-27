c9d707f6838f88ee5430e6ec3a94b06a7a986d1c
antham <hamonanth@gmail.com>
2013-05-27 20:04:02 +0200

    [Library] Remove moz

--- config/misc.el
+++ config/misc.el
@@ -4,18 +4,12 @@
   (interactive)
   (let ((current-value (frame-parameter nil 'fullscreen)))
     (set-frame-parameter nil 'fullscreen
-			 (if (equal 'fullboth current-value)
-			     (if (boundp 'old-fullscreen) old-fullscreen nil)
-			   (progn (setq old-fullscreen current-value)
-				  'fullboth)))))
+                         (if (equal 'fullboth current-value)
+                             (if (boundp 'old-fullscreen) old-fullscreen nil)
+                           (progn (setq old-fullscreen current-value)
+                                  'fullboth)))))
 
 (display-time-mode t)
-
-(autoload 'moz-minor-mode "moz" "Mozilla Minor and Inferior Mozilla Modes" t)
-
-(add-hook 'js2-mode-hook 'js2-custom-setup)
-(defun js2-custom-setup ()
-  (moz-minor-mode 1))
 
 (setq browse-url-browser-function 'browse-url-generic
       browse-url-generic-program "/usr/bin/conkeror")
