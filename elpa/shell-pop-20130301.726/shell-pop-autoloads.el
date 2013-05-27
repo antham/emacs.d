2e3592d7fa22bbeebe397125db1e113c06e0d05f
antham <hamonanth@gmail.com>
2013-05-27 23:18:41 +0200

    [Package manager] Replace shell-pop

---
+++ elpa/shell-pop-20130301.726/shell-pop-autoloads.el
@@ -0,0 +1,18 @@
+;;; shell-pop-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("shell-pop-pkg.el" "shell-pop.el") (20899
+;;;;;;  52432 454835 188000))
+
+;;;***
+
+(provide 'shell-pop-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; shell-pop-autoloads.el ends here
