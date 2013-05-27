ece10a0c02d46d4887d69294c040b3f9fa8f133e
antham <hamonanth@gmail.com>
2013-05-27 19:36:38 +0200

    [Package manager] Add melpa

--- init.el
+++ init.el
@@ -3,8 +3,14 @@
 
 (require 'package)
 (add-to-list 'package-archives
-    '("marmalade" .
-      "http://marmalade-repo.org/packages/"))
+             '("marmalade" .
+               "http://marmalade-repo.org/packages/") t)
+(add-to-list 'package-archives
+             '("melpa" .
+               "http://melpa.milkbox.net/packages/") t)
+
+
+
 (package-initialize)
 
 (add-hook 'after-init-hook (lambda () (load-file "~/.emacs.d/config/init.el")))
