b92d14da1457a8e099d401129ea5fed71ea2abc8
antham <hamonanth@gmail.com>
2013-06-04 22:59:42 +0200

    [Package manager] Change xcscope for acscope

---
+++ elpa/ascope-20130328.1110/ascope-autoloads.el
@@ -0,0 +1,18 @@
+;;; ascope-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("ascope-pkg.el" "ascope.el") (20910 21439
+;;;;;;  760619 383000))
+
+;;;***
+
+(provide 'ascope-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; ascope-autoloads.el ends here
