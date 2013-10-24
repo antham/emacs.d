2e0eb17a67cbd445a2d0f53412e16008b20cb596
antham <hamonanth@gmail.com>
2013-10-24 23:24:17 +0200

    [Library] Update

---
+++ elpa/ascope-20130824.1158/ascope-autoloads.el
@@ -0,0 +1,18 @@
+;;; ascope-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("ascope-pkg.el" "ascope.el") (21097 36301
+;;;;;;  700060 1000))
+
+;;;***
+
+(provide 'ascope-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; ascope-autoloads.el ends here
