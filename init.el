0ec495c2e9db0b75b6dbfa8896686b0e322ecc5c
antham <hamonanth@gmail.com>
2013-11-16 19:12:48 +0100

    [Init] Remove useless config/init.el, use defaut

--- init.el
+++ init.el
@@ -1,5 +1,20 @@
 ;; Load packages
 (load-file "~/.emacs.d/config/package.el")
 
-;; Load init
-(add-hook 'after-init-hook (lambda() (load-file "~/.emacs.d/config/init.el")))
+;; Load custom file
+(setq custom-file "~/.emacs.d/config/custom.el")
+(load custom-file)
+
+;; Functions
+(load-file "~/.emacs.d/functions/indent.el")
+(load-file "~/.emacs.d/functions/file.el")
+(load-file "~/.emacs.d/functions/misc.el")
+(load-file "~/.emacs.d/functions/term.el")
+
+;; Configuration
+(load-file "~/.emacs.d/config/def.el")
+(load-file "~/.emacs.d/config/key.el")
+
+(if (file-exists-p "~/.emacs.d/config/perso.el")
+    (load-file "~/.emacs.d/config/perso.el")
+  )
