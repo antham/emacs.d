e388dab3bc8cc8096930749d53537b0cc7bf11e3
antham <hamonanth@gmail.com>
2013-11-22 12:59:54 +0100

    [Key] Change tag binding

--- config/key.el
+++ config/key.el
@@ -1,7 +1,7 @@
 (global-set-key "\C-x\C-m" 'execute-extended-command)
 (global-set-key "\C-c\C-m" 'execute-extended-command)
 
-(global-set-key (kbd "M-.") 'helm-gtags-select)
+(global-set-key (kbd "M-.") 'helm-gtags-find-tag-from-here)
 (global-set-key (kbd "C-;") 'file-helm)
 (global-set-key (kbd "C-,") 'buffer-helm)
 (global-set-key (kbd "C-x C-f") 'helm-find-files)
