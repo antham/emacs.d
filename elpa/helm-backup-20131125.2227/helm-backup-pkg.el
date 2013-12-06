84a619d1909ad9a6e7bc6918a7350181c4e49688
antham <hamonanth@gmail.com>
2013-12-06 10:09:17 +0100

    [Package] Update, add missing package

---
+++ elpa/helm-backup-20131125.2227/helm-backup-pkg.el
@@ -0,0 +1,1 @@
+(define-package "helm-backup" "20131125.2227" "Backup each file change using git" (quote ((helm "1.5.5") (s "1.8.0"))))
--- elpa/helm-gtags-20131121.1405/helm-gtags-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "helm-gtags" "20131121.1405" "GNU GLOBAL helm interface" (quote ((helm "1.0"))))
