e2b934927984a0c6faed3f9bb659c14e9a681e2e
antham <hamonanth@gmail.com>
2015-06-28 22:06:43 +0200

    Use helm-M-x instead of execute-extended-command

--- config/key.el
+++ config/key.el
@@ -1,5 +1,5 @@
-(global-set-key "\C-x\C-m" 'execute-extended-command)
-(global-set-key "\C-c\C-m" 'execute-extended-command)
+(global-set-key "\C-x\C-m" 'helm-M-x)
+(global-set-key "\C-c\C-m" 'helm-M-x)
 
 (global-set-key (kbd "M-.") 'helm-gtags-find-tag-from-here)
 (global-set-key (kbd "C-;") 'file-helm)
