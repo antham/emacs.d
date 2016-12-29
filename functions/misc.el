20a28159a6774fbd92d2a88e1f5bd85d05e26dca
antham <hamonanth@gmail.com>
2016-12-29 18:45:27 +0100

    Set jsx mode

--- functions/misc.el
+++ functions/misc.el
@@ -8,3 +8,11 @@
                        (search-forward (char-to-string char) nil nil arg)
                      (backward-char direction))
                    (point)))))
+
+(require 'web-mode)
+(add-to-list 'auto-mode-alist '("\\.jsx\\'" . web-mode))
+
+(add-hook 'web-mode-hook
+          (lambda ()
+            (web-mode-set-content-type "jsx")
+            (message "now set to: %s" web-mode-content-type)))
